b51215770cf2be2f1eb2750692159e0bfef4c421
Samim Shammo <you@example.comsamimshammo>
2021-09-19 22:39:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+ 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <DisplyBlog></DisplyBlog>
+       <BatteryDown></BatteryDown>
+    </div>
+  );
+}
+
+function BatteryDown(){
+  const [down, setBattery] = useState(100)
+  const Batterydown = () =>{
+    if(down > 0){
+       const newBattery = down - 10;
+      setBattery(newBattery)
+    }
+  }
+
+  return (
+    <div>
+      <h2>{down}</h2>
+      <button onClick={Batterydown}>BatteryDown</button>
+    </div>
+  );
+}
+
+function DisplyBlog(){
+  const [picture, myPicture] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(res => res.json())
+    .then(data => myPicture(data))
+  },[])
+
+  
+  return (
+    <div >
+        {/* {
+         picture.map(pic => <Blog  title={pic.title} completed={pic.completed}></Blog>)
+        } */}
+       
+    </div>
+  );
+}
+
+function Blog(props){
+  
+  return (
+    <div>
+      <h1>title{props.title}</h1>
+      <h2>Completed:{props.completed}</h2>
+
+      
+      
     </div>
   );
 }
